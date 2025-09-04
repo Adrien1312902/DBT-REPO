83b46771a14965fb472fb8fc0d50f4c8925c000e
Adrien1312902 <alix.lavoipierre@gmail.com>
2025-09-04 16:16:03 +0200

    Update fact_music.sql

--- models/fact_music.sql
+++ models/fact_music.sql
@@ -1,18 +1,20 @@
 SELECT
-  t.trackid,
-  t.name AS track_name,
-  a.title AS album_title,
-  ar.name AS artist_name,
-  g.name AS genre,
-  m.name AS mediatype,
-  t.composer,
-  t.milliseconds,
-  t.bytes,
-  t.unitprice
+    t.trackid,
+    t.name AS track_name,
+    a.title AS album_title,
+    ar.name AS artist_name,
+    g.name AS genre,
+    m.name AS mediatype,
+    t.composer,
+    t.milliseconds,
+    t.bytes,
+    t.unitprice
+
 FROM {{ ref('track') }} t
 LEFT JOIN {{ ref('album') }} a ON t.albumid = a.albumid
 LEFT JOIN {{ ref('artist') }} ar ON a.artistid = ar.artistid
 LEFT JOIN {{ ref('genre') }} g ON t.genreid = g.genreid
 LEFT JOIN {{ ref('mediatype') }} m ON t.mediatypeid = m.mediatypeid
+
 
 
