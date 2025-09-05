a889ff1edeac3af663e65c601d93f46d81a923d1
Adrien1312902 <alix.lavoipierre@gmail.com>
2025-09-05 15:33:05 +0200

    Update morceaux_artist_fr.sql

--- models/star/morceaux_artist_fr.sql
+++ models/star/morceaux_artist_fr.sql
@@ -2,7 +2,7 @@
 
 SELECT
   t.Name AS track_name,
-  a.Name AS artist_name,
+  ar.Name AS artist_name,
   ar.Country
 FROM {{ source('music_source', 'track') }} t
 JOIN {{ source('music_source', 'genre') }} g ON t.GenreId = g.GenreId
@@ -10,4 +10,5 @@ JOIN {{ source('music_source', 'album') }} al ON t.AlbumId = al.AlbumId
 JOIN {{ source('music_source', 'artist') }} ar ON al.ArtistId = ar.ArtistId
 WHERE g.Name = 'Rock'
   AND ar.Country = 'France'
+
 
