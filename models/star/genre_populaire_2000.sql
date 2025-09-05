6302a4b0212c29fad7517d3cfedd466cd7f89f24
Adrien1312902 <alix.lavoipierre@gmail.com>
2025-09-05 14:59:34 +0200

    Update genre_populaire_2000.sql

--- models/star/genre_populaire_2000.sql
+++ models/star/genre_populaire_2000.sql
@@ -1,8 +1,11 @@
-{{ config(materialized='view') }}
-
-SELECT genre, COUNT(*) AS total
-FROM {{ ref('fact_music') }}
-WHERE YEAR(composer) BETWEEN 2000 AND 2009
-GROUP BY genre
-ORDER BY total DESC
+SELECT
+  g.Name AS genre,
+  COUNT(*) AS play_count
+FROM {{ source('music_source', 'track') }} t
+JOIN {{ source('music_source', 'genre') }} g ON t.GenreId = g.GenreId
+JOIN {{ source('music_source', 'album') }} a ON t.AlbumId = a.AlbumId
+WHERE a.ProducedYear = 2000
+GROUP BY g.Name
+ORDER BY play_count DESC
 LIMIT 1
+
