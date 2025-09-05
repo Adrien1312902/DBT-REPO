cb1f7ded7fa718f7e23c70d550a34c191a6bb02b
Adrien1312902 <alix.lavoipierre@gmail.com>
2025-09-05 15:25:32 +0200

    Update genre_populaire_2000.sql

--- models/star/genre_populaire_2000.sql
+++ models/star/genre_populaire_2000.sql
@@ -6,7 +6,7 @@ SELECT
 FROM {{ source('music_source', 'track') }} t
 JOIN {{ source('music_source', 'genre') }} g ON t.GenreId = g.GenreId
 JOIN {{ source('music_source', 'album') }} a ON t.AlbumId = a.AlbumId
-WHERE a.ProducedYear = 2000
+WHERE a.Prod_year = 2000
 GROUP BY g.Name
 ORDER BY play_count DESC
 LIMIT 1
